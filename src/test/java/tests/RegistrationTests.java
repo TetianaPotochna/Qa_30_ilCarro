28e33e6b9e36b4c4e08801e11e67c4640168c052
TetianaPotochna <t.potochna@gmail.com>
2025-08-15 10:49:34 +0300

    update

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -1,7 +1,6 @@
 package tests;
 
 import models.User;
-import org.openqa.selenium.Alert;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -170,5 +169,6 @@ public class RegistrationTests extends TestBase {
     @AfterMethod
     public void postCondition() {
         app.getHelperUser().clickOkButton();
+        app.getHelperUser().checkPolicy();
     }
 }
